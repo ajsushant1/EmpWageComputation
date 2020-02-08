6d2ff6a40a9cc45ee6312237b51a87060403dab1
ajsushant1 <sushantajgaonkar2766@gmail.com>
2020-02-08 16:14:05 +0530

    ADDED: Parttime employee and wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -3,6 +3,7 @@
 echo "/********************* WELCOME TO EMPLOYEE WAGE COMPUTATION *********************/"
 
 # CONSTANTS
+IS_PARTTIME=1
 IS_FULLTIME=2
 EMP_WAGE_RATE_PER_HR=20
 
@@ -12,10 +13,13 @@ randomCheck=$((RANDOM%3))
 # CHECKING CONDITION AND GETTING EMPLOYEE HOURS
 if [ $IS_FULLTIME -eq $randomCheck ]
 then
-	echo "Employee is Present"
 	employeeHrs=8
+
+elif [ $IS_PARTTIME -eq $randomCheck ]
+then
+	employeeHrs=4
+
 else
-	echo "Employee is Absent"
 	employeeHrs=0
 fi
 

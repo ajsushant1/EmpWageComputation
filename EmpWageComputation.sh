5e41c6be5ed3149db1e41d77a7190427489e844f
ajsushant1 <sushantajgaonkar2766@gmail.com>
2020-02-08 17:58:36 +0530

    ADDED: Calculating wages for a month of employee

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -6,12 +6,20 @@ echo "/********************* WELCOME TO EMPLOYEE WAGE COMPUTATION **************
 IS_PARTTIME=1
 IS_FULLTIME=2
 EMP_WAGE_RATE_PER_HR=20
+NUM_OF_WORKING_DAYS=20
+
+# VARIABLES
+totalEmpSalary=0
+
+# LOOP FOR CALCULATING WAGES FOR MONTH AND TOTAL EMPLOYEE SALARY
+for (( day=1; day<=$NUM_OF_WORKING_DAYS; day++ ))
+do
 
 # GENERATING RANDOM VALUE
-randomCheck=$((RANDOM%3))
+	randomCheck=$((RANDOM%3))
 
-# CHECKING CONDITION AND GETTING EMPLOYEE HOURS
-case $randomCheck in
+# CHECKING CASE AND GETTING EMPLOYEE HOURS
+	case $randomCheck in
 		$IS_FULLTIME)
 				employeeHrs=8
 					;;
@@ -23,7 +31,11 @@ case $randomCheck in
 		*)
 				employeeHrs=0
 					;;
-esac
+	esac
 
 # CALCULATING EMPLOYEE DAILY WAGE
 employeeDailyWage=$(($employeeHrs*$EMP_WAGE_RATE_PER_HR))
+
+# CALCULATING EMPLOYEE TOTAL SALARY
+totalEmpSalary=$(($totalEmpSalary+$employeeDailyWage))
+done

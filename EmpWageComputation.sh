ffc27e111e2d1c1f26c261dcf4219336ca6836d5
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-14 18:14:28 +0530

    MODIFIED: Indentation of code

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -13,16 +13,14 @@ randomCheck=$((RANDOM%3))
 # CHECKING CONDITION AND GETTING EMPLOYEE HOURS
 case $randomCheck in
 		$IS_FULLTIME)
-				employeeHrs=8
-					;;
-
+			employeeHrs=8
+				;;
 		$IS_PARTTIME)
-				employeeHrs=4
-					;;
-
+			employeeHrs=4
+				;;
 		*)
-				employeeHrs=0
-					;;
+			employeeHrs=0
+				;;
 esac
 
 # CALCULATING EMPLOYEE DAILY WAGE
